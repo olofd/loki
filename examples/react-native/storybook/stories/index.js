eae44af9decad81def644c28ccd6653cdf36de43
Olof Dahlbom <olof.dahlbom@me.com>
2017-12-07 10:22:40 +0100

    Inisde out

--- examples/react-native/storybook/stories/index.js
+++ examples/react-native/storybook/stories/index.js
@@ -1,45 +1,45 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions */
 
-import React from 'react';
-import { Text } from 'react-native';
+import React from "react";
+import { Text } from "react-native";
 
-import { storiesOf } from '@storybook/react-native';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react-native";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
 
-import Button from './Button';
-import CenterView from './CenterView';
-import Welcome from './Welcome';
-import Logo from './Logo';
-import ErrorThrowingComponent from './ErrorThrowingComponent';
+import Button from "./Button";
+import CenterView from "./CenterView";
+import Welcome from "./Welcome";
+import Logo from "./Logo";
+import ErrorThrowingComponent from "./ErrorThrowingComponent";
+import ViewShot from "react-native-view-shot";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => <Welcome showApp={linkTo("Button")} />);
 
-storiesOf('Button', module)
+storiesOf("Button", module)
   .addDecorator(getStory => <CenterView>{getStory()}</CenterView>)
-  .add('with text', () =>
-    <Button onPress={action('clicked-text')}>
+  .add("with text", () => (
+    <Button onPress={action("clicked-text")}>
       <Text>Hello Button</Text>
     </Button>
-  )
-  .add('with some emoji', () =>
-    <Button onPress={action('clicked-emoji')}>
+  ))
+  .add("with some emoji", () => (
+    <Button onPress={action("clicked-emoji")}>
       <Text>😀 😎 👍 💯</Text>
     </Button>
-  )
-  .add.skip('skipped story', () =>
-    <Button onPress={action('clicked-emoji')}>
-      <Text>I am skipped</Text>
-    </Button>
-  );
+  ));
+// .add.skip('skipped story', () =>
+//   <Button onPress={action('clicked-emoji')}>
+//     <Text>I am skipped</Text>
+//   </Button>
+// );
 
-storiesOf('Logo', module)
-  .add('without delay', () => <Logo />)
-  .add('with 15s delay', () => <Logo delay={15000} />);
+storiesOf("Logo", module).add("without delay", () => <Logo />);
+// .add("with 15s delay", () => <Logo delay={15000} />);
 
-storiesOf('Error Handling', module)
-  .add('with ErrorThrowingComponent', () => <ErrorThrowingComponent />)
-  .add('with console.warn', () => {
-    console.warn('This warning should not show up in the screenshot');
+storiesOf("Error Handling", module)
+  // .add("with ErrorThrowingComponent", () => <ErrorThrowingComponent />)
+  .add("with console.warn", () => {
+    console.warn("This warning should not show up in the screenshot");
     return <Text>This story emits a console.warn</Text>;
   });
--- examples/react-native/storybook/storybook.js
+++ examples/react-native/storybook/storybook.js
@@ -1,14 +1,43 @@
 /* eslint-disable import/no-extraneous-dependencies, import/no-unresolved, import/extensions, global-require */
 
-import { AppRegistry } from 'react-native';
-import { getStorybookUI, configure } from '@storybook/react-native';
-import 'loki/configure-react-native';
+import { AppRegistry, View } from "react-native";
+import React, { Component } from "react";
+import { getStorybookUI, configure } from "@storybook/react-native";
+import "loki/configure-react-native";
+import ViewShot from "react-native-view-shot";
+import { captureScreen } from "react-native-view-shot";
 
 // import stories
 configure(() => {
-  require('./stories');
+  require("./stories");
 }, module);
+const StorybookUI = getStorybookUI({ port: 7007, host: "localhost" });
 
-const StorybookUI = getStorybookUI({ port: 7007, host: 'localhost' });
-AppRegistry.registerComponent('Loki', () => StorybookUI);
+class MyWrapper extends Component {
+  componentDidMount() {
+    global.saveScreen = cb => {
+      cb();
+      captureScreen({
+        format: "png",
+        result: "base64",
+      }).then(
+        uri => {
+          console.log("Image saved to");
+          cb();
+        },
+        error => console.error("Oops, snapshot failed", error)
+      );
+    };
+  }
+
+  render() {
+    return (
+      <View ref={() => this.ref} style={{ flex: 1 }}>
+        <StorybookUI />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent("Loki", () => MyWrapper);
 export default StorybookUI;
